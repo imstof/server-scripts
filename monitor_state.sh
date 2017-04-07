eb69d9ff605e9f95dd009933158a3cb7c0d68442
imstof <imstof@c3ddb01.cm.cluster>
2017-04-07 14:12:15 -0400

    tweak monitor_state

--- monitor_state.sh
+++ monitor_state.sh
@@ -4,6 +4,7 @@
 #		0 20,0,4 * * 1-5 /home/imstof/bin/monitor_state [node number]
 
 scontrol -a show node node$1 | grep State=IDLE+DRAIN > ~/statecheck$1.txt
+scontrol -a show node node$1 | grep State=DOWN*+DRAIN > ~/statecheck$1.txt
 if [[ -n $(cat statecheck$1.txt) ]]
 then 
 	echo "Node"$1" ready." | mail -s "node"$1" ready." -a ~/statecheck$1.txt cehnstrom@techsquare.com
