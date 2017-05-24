384826f5c495f465372ebfc41eddabe03cd54337
imstof <imstof@c3ddb01.cm.cluster>
2017-05-24 05:44:52 -0400

    ;asdlkfj

--- imstof-slurm-node-status.sh
+++ imstof-slurm-node-status.sh
@@ -30,21 +30,21 @@ done
 
 # pull reason,user,timestamp,state,node from sinfo
 # SED-CUT IDLE+ DOWN+
-sinfo -N -o '%E:%u:%H:%t:%N' | grep -e down -e drain -e drng -e maint > ~/.temp/sinfo_out.txt
+sinfo -N -o '%E=%u=%H=%t=%N' | grep -e down -e drain -e drng -e maint > ~/.temp/sinfo_out.txt
 
 # format output in file for email or echo
 echo $(date +"%Y-%m-%d") >> ~/.temp/status_report.txt
-echo  >> ~/.temp/status_report.txt
-echo "=== DOWN ===" >> ~/.temp/~/.temp/status_report.txt
-cat ~/.temp/sinfo_out.txt | awk -F ":" '/down/ {printf "%-21.20s%-13.10s%-20s%-7s%s.\n", $1,$2,$3,$4,$5}' > ~/.temp/status_report.txt
-echo  >> ~/.temp/status_report.txt
+echo >> ~/.temp/status_report.txt
+echo "=== DOWN ===" >> ~/.temp/status_report.txt
+cat ~/.temp/sinfo_out.txt | awk -F "=" '/down/ {printf "%-21.20s%-13.10s%-20s%-7s%s.\n", $1,$2,$3,$4,$5}' >> ~/.temp/status_report.txt
+echo >> ~/.temp/status_report.txt
 echo "=== DRAIN WITH ISSUES ===" >> ~/.temp/status_report.txt
-cat ~/.temp/sinfo_out.txt | awk -F ":" '/drain|drng|maint/ {printf "%-21.20s%-13.10s%-20s%-7s%s\n", $1,$2,$3,$4,$5}'
-echo  >> ~/.temp/status_report.txt
+cat ~/.temp/sinfo_out.txt | awk -F "=" '/drain|drng|maint/ {printf "%-21.20s%-13.10s%-20s%-7s%s\n", $1,$2,$3,$4,$5}'
+echo >> ~/.temp/status_report.txt
 echo "=== DRAIN ON PURPOSE ===" >> ~/.temp/status_report.txt
 # CRITERI[A,ON] FOR ON PURPOSE?
 
-echo  >> ~/.temp/status_report.txt
+echo >> ~/.temp/status_report.txt
 echo "=== Jobs stuck in CG State ===" >> ~/.temp/status_report.txt
 
 # send report to mail-list or echo
