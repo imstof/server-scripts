fd60743f3b6d3e65ebedc1ebd1176c358baf4183
imstof <imstof@c3ddb01.cm.cluster>
2017-06-05 16:31:24 -0400

    load status 1.0

---
+++ imstof-slurm-node-load.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# rewrite of slurm load script
+
+# help func
+show_help() {
+	echo
+	echo "Usage: `basename $0` [option...]"
+	echo "Generate report of nodes with high cpu loads"
+	echo
+	echo "	-t	echo to terminal (no mail)"
+	echo "	-h	disply this help and exit"
+	echo
+}
+
+# set opts
+nomail=false
+while getopts :ht opt
+do
+	case $opt in
+		t)
+			nomail=true
+			;;
+		h)
+			show_help
+			exit 0
+			;;
+		\?)
+			echo "Invalid option $OPTARG"
+			echo "Try '`basename $0` -h' for help"
+			exit 1
+			;;
+	esac
+done
+
+# pull nodename,cpus,load,state to file
+sinfo -h -t alloc,idle -o %n=%C/=%O=%T | cut -d'/' -f1,5 | sed 's/\///' >> /tmp/load_data.txt
+
+# report problem nodes
+echo $(date +"%Y-%m-%d") >> /tmp/load_report.txt
+echo
+echo " NODE  CORES  LOAD    STATE" >> /tmp/load_report.txt
+echo "============================" >> /tmp/load_report.txt
+echo
+
+for i in $(cat /tmp/load_data.txt)
+do
+	cores=$(echo $i | awk -F "=" '{printf "%s\n", $2}')
+	load=$(echo $i | awk -F "=" '{printf "%s\n", $3}')
+	state=$(echo $i | awk -F "=" '{printf "%s\n", $4}')
+# report if load is greater than 10 on idle node
+	if [[ $(echo $load '>' 10 | bc) == 1 && $cores == 0 ]]
+	then
+		echo $i | awk -F "=" '{printf "%-9s%-4s%1-10s%s\n",$1,$2,$3,$4}' >> /tmp/load_report.txt
+# report if load is greater than 100
+	elif [[ $(echo $load '>' 100 | bc) == 1 ]]
+	then
+		echo $i | awk -F "=" '{printf "%-9s%-4s%1-10s%s\n",$1,$2,$3,$4}' >> /tmp/load_report.txt
+	fi
+done
+
+# display or mail report
+if ( $nomail == true )
+then
+	cat /tmp/load_report.txt
+else
+	mail -s	"slurm-daily-node-load LOAD $(hostname) $(date +"%Y-%m-%d")" cehnstrom@techsquare.com < /tmp/status_report.txt
+fi
+
+# remove tmp files
+rm /tmp/load_data.txt
+rm /tmp/load_report.txt
--- imstof-slurm-node-status.sh
+++ imstof-slurm-node-status.sh
@@ -14,12 +14,12 @@ show_help () {
 }
 
 # set variable for -t flag
-NOMAIL=false
+nomail=false
 while getopts :ht opt
 do
 	case $opt in
 		t)
-			NOMAIL=true
+			nomail=true
 			;;
 		h)
 			show_help
@@ -34,36 +34,36 @@ do
 done
 
 # pull reason,user,timestamp,state,node from sinfo
-sinfo -o '%E=%u=%H=%t=%N' | grep -e down -e drain -e drng -e maint > ~/.temp/sinfo_out.txt
+sinfo -o '%E=%u=%H=%t=%N' | grep -e down -e drain -e drng -e maint > /tmp/sinfo_out.txt
 
 # format output in file for email or echo
-echo $(date +"%Y-%m-%d") >> ~/.temp/status_report.txt
-echo >> ~/.temp/status_report.txt
-echo "=== DOWN ===" >> ~/.temp/status_report.txt
-cat ~/.temp/sinfo_out.txt | awk -F "=" '/down/ {printf "%-23.20s%-7.5s%-22s%-8s%s.\n", $1,$2,$3,$4,$5}' >> ~/.temp/status_report.txt
-echo >> ~/.temp/status_report.txt
-echo "=== DRAIN WITH ISSUES ===" >> ~/.temp/status_report.txt
-cat ~/.temp/sinfo_out.txt | awk -F "=" '/drain|drng|maint/ {if ($1 != toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> ~/.temp/status_report.txt
-echo >> ~/.temp/status_report.txt
-echo "=== DRAIN ON PURPOSE ===" >> ~/.temp/status_report.txt
-cat ~/.temp/sinfo_out.txt | awk -F "=" '/drain|drng|maint/ {if ($1 == toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> ~/.temp/status_report.txt
-echo >> ~/.temp/status_report.txt
-echo "=== Jobs stuck in CG State ===" >> ~/.temp/status_report.txt
+echo $(date +"%Y-%m-%d") >> /tmp/status_report.txt
+echo >> /tmp/status_report.txt
+echo "=== DOWN ===" >> /tmp/status_report.txt
+cat /tmp/sinfo_out.txt | awk -F "=" '/down/ {printf "%-23.20s%-7.5s%-22s%-8s%s.\n", $1,$2,$3,$4,$5}' >> /tmp/status_report.txt
+echo >> /tmp/status_report.txt
+echo "=== DRAIN WITH ISSUES ===" >> /tmp/status_report.txt
+cat /tmp/sinfo_out.txt | awk -F "=" '/drain|drng|maint/ {if ($1 != toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> /tmp/status_report.txt
+echo >> /tmp/status_report.txt
+echo "=== DRAIN ON PURPOSE ===" >> /tmp/status_report.txt
+cat /tmp/sinfo_out.txt | awk -F "=" '/drain|drng|maint/ {if ($1 == toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> /tmp/status_report.txt
+echo >> /tmp/status_report.txt
+echo "=== Jobs stuck in CG State ===" >> /tmp/status_report.txt
 #pipe through awk in case job_name has "CG" in string
 #test other than any CG at time script runs?
-#squeue | awk '$5 == "CG"' >> ~/.temp/status_report.txt
-squeue | grep CG | tr -s ' ' | awk '{printf "%-9s%-11.9s%-12.10s%-10.8s%-4s%-6s%s\n",$1,$2,$3,$4,$5,$6,$8}' >> ~/.temp/status_report.txt
-echo >> ~/.temp/status_report.txt
-echo "=== Active Reservations ===" >> ~/.temp/status_report.txt
-sinfo -T | grep ACTIVE >> ~/.temp/status_report.txt
+#squeue | awk '$5 == "CG"' >> /tmp/status_report.txt
+squeue | grep CG | tr -s ' ' | awk '{printf "%-9s%-11.9s%-12.10s%-10.8s%-4s%-9.7s%s\n",$1,$2,$3,$4,$5,$6,$8}' >> /tmp/status_report.txt
+echo >> /tmp/status_report.txt
+echo "=== Active Reservations ===" >> /tmp/status_report.txt
+sinfo -T | grep ACTIVE >> /tmp/status_report.txt
 
 # send report to mail-list or echo
-if [[ $NOMAIL == true ]]
+if [[ $nomail == true ]]
 then
-	cat ~/.temp/status_report.txt
+	cat /tmp/status_report.txt
 else
-	mail -s "slurm-daily-node-status STATUS $(hostname) $(date +"%Y-%m-%d")" cehnstrom@techsquare.com < ~/.temp/status_report.txt
+	mail -s "slurm-daily-node-status STATUS $(hostname) $(date +"%Y-%m-%d")" cehnstrom@techsquare.com < /tmp/status_report.txt
 fi
 
-rm ~/.temp/sinfo_out.txt
-rm ~/.temp/status_report.txt
+rm /tmp/sinfo_out.txt
+rm /tmp/status_report.txt
