6d9dbdb1b1c13f2f85579ff260889ff34f33a82b
imstof <cehnstrom@techsquare.com>
2017-06-21 22:28:13 -0400

    bam

--- imstof-slurm-node-status-II.sh
+++ imstof-slurm-node-status-II.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-#define paths!
+#define paths
+export PATH=$PATH:/usr/bin/
 
 # slurm daily node status rewrite
 
+# create temp file(s)
+info_file=$(mktemp `basename $0`.XXX.txt)
+report_file=$(mktemp `basename $0`.XXX.txt)
+
 #help func
-show_help () {
+show_help() {
 	echo
 	echo "Usage: `basename $0` [option...]"
 	echo "Generate report of problem nodes and mail to admin"
@@ -12,6 +17,12 @@ show_help () {
 	echo "   -t	echo to terminal (no mail)"
 	echo "   -h	display this help and exit"
 	echo
+}
+
+#trap action func
+cleanup() {
+	rm $info_file
+	rm $report_file
 }
 
 # set variable for -t flag
@@ -34,46 +45,40 @@ do
 	esac
 done
 
-# create temp file(s)
-file0=$(mktemp $())
 
 # pull reason,user,timestamp,state,node from sinfo
 
-# use -t for states!
-# use mktemp!
 # capture sterr!
 
-sinfo -o '%E=%u=%H=%t=%N' | grep -e down -e drain -e drng -e maint > /tmp/sinfo_out.txt
+sinfo -t down,drain,drng,maint -o '%E=%u=%H=%t=%N' > $info_file
 
 # format output in file for email or echo
-echo $(date +"%Y-%m-%d") >> /tmp/status_report.txt
-echo >> /tmp/status_report.txt
-echo "=== DOWN ===" >> /tmp/status_report.txt
-cat /tmp/sinfo_out.txt | awk -F "=" '/down/ {printf "%-23.20s%-7.5s%-22s%-8s%s.\n", $1,$2,$3,$4,$5}' >> /tmp/status_report.txt
-echo >> /tmp/status_report.txt
-echo "=== DRAIN WITH ISSUES ===" >> /tmp/status_report.txt
-cat /tmp/sinfo_out.txt | awk -F "=" '/drain|drng|maint/ {if ($1 != toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> /tmp/status_report.txt
-echo >> /tmp/status_report.txt
-echo "=== DRAIN ON PURPOSE ===" >> /tmp/status_report.txt
-cat /tmp/sinfo_out.txt | awk -F "=" '/drain|drng|maint/ {if ($1 == toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> /tmp/status_report.txt
-echo >> /tmp/status_report.txt
-echo "=== Jobs stuck in CG State ===" >> /tmp/status_report.txt
-#pipe through awk in case job_name has "CG" in string
-#test other than any CG at time script runs?
-#squeue | awk '$5 == "CG"' >> /tmp/status_report.txt
-squeue | grep CG | tr -s ' ' | awk '{printf "%-9s%-11.9s%-12.10s%-10.8s%-4s%-9.7s%s\n",$1,$2,$3,$4,$5,$6,$8}' >> /tmp/status_report.txt
-echo >> /tmp/status_report.txt
-echo "=== Active Reservations ===" >> /tmp/status_report.txt
-sinfo -T | grep ACTIVE >> /tmp/status_report.txt
+echo $(date +"%Y-%m-%d") >> $report_file
+echo >> $report_file
+echo "=== DOWN ===" >> $report_file
+cat $info_file | awk -F "=" '/down/ {printf "%-23.20s%-7.5s%-22s%-8s%s.\n", $1,$2,$3,$4,$5}' >> $report_file
+echo >> $report_file
+echo "=== DRAIN WITH ISSUES ===" >> $report_file
+cat $info_file | awk -F "=" '/drain|drng|maint/ {if ($1 != toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> $report_file
+echo >> $report_file
+echo "=== DRAIN ON PURPOSE ===" >> $report_file
+cat $info_file | awk -F "=" '/drain|drng|maint/ {if ($1 == toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> $report_file
+echo >> $report_file
+echo "=== Jobs stuck in CG State ===" >> $report_file
+#pipe through awk in case job_name has "CG" in string?
+#squeue | awk '$5 == "CG"' >> $report_file
+squeue | grep CG | tr -s ' ' | awk '{printf "%-9s%-11.9s%-12.10s%-10.8s%-4s%-9.7s%s\n",$1,$2,$3,$4,$5,$6,$8}' >> $report_file
+echo >> $report_file
+echo "=== Active Reservations ===" >> $report_file
+sinfo -T | grep ACTIVE >> $report_file
 
 # send report to mail-list or echo
 if [[ $nomail == true ]]
 then
-	cat /tmp/status_report.txt
+	cat $report_file
 else
-	mail -s "slurm-daily-node-status STATUS $(hostname) $(date +"%Y-%m-%d")" cehnstrom@techsquare.com < /tmp/status_report.txt
+	mail -s "slurm-daily-node-status STATUS $(hostname) $(date +"%Y-%m-%d")" cehnstrom@techsquare.com < $report_file
 fi
 
 # trap statement to rm temp files on exit
-rm /tmp/sinfo_out.txt
-rm /tmp/status_report.txt
+trap cleanup EXIT
--- mktemp-trap-test.sh
+++ mktemp-trap-test.sh
@@ -7,7 +7,7 @@ cleanup(){
 	rm $file1
 }
 
-file0=$(mktemp `basename $0`.XXXXX)
+file0=$(mktemp `basename $0`.XXXXX.txt)
 file1=$(mktemp `basename $0`.XXXXX)
 
 echo "this file is named: "$file0
