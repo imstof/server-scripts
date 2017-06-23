12cfd5aa4a8daa75b65d5fff315b5e56ded8546b
imstof <cehnstrom@techsquare.com>
2017-06-23 17:35:34 -0400

    notes tweaked

--- imstof-slurm-node-status-II.sh
+++ imstof-slurm-node-status-II.sh
@@ -4,10 +4,6 @@ export PATH=$PATH:/usr/bin/
 
 # slurm daily node status rewrite
 
-# create temp file(s)
-info_file=$(mktemp `basename $0`.XXX.txt)
-report_file=$(mktemp `basename $0`.XXX.txt)
-
 #help func
 show_help() {
 	echo
@@ -20,13 +16,19 @@ show_help() {
 }
 
 #trap action func
-cleanup() {
-	rm $info_file
-	rm $report_file
-}
+# using temp dir and single action trap instead
+#cleanup() {
+#	rm $info_file
+#	rm $report_file
+#}
+
+#variables and temp files
+nomail=false
+dir0=$(mktemp -d `basename $0`.XXX)
+info_file=$(mktemp $dir0/XXX.txt)
+report_file=$(mktemp $dir0/XXX.txt)
 
 # set variable for -t flag
-nomail=false
 while getopts :ht opt
 do
 	case $opt in
@@ -48,29 +50,28 @@ done
 
 # pull reason,user,timestamp,state,node from sinfo
 
-# capture sterr!
-
-sinfo -t down,drain,drng,maint -o '%E=%u=%H=%t=%N' > $info_file
+sinfo -t down,drain,drng,maint -o '%E=%u=%H=%t=%N' > $info_file 2>&1
 
 # format output in file for email or echo
 echo $(date +"%Y-%m-%d") >> $report_file
 echo >> $report_file
 echo "=== DOWN ===" >> $report_file
-cat $info_file | awk -F "=" '/down/ {printf "%-23.20s%-7.5s%-22s%-8s%s.\n", $1,$2,$3,$4,$5}' >> $report_file
+cat $info_file | awk -F "=" '/down/ {printf "%-23.20s%-7.5s%-22s%-8s%s.\n", $1,$2,$3,$4,$5}' >> $report_file 2>&1
 echo >> $report_file
 echo "=== DRAIN WITH ISSUES ===" >> $report_file
-cat $info_file | awk -F "=" '/drain|drng|maint/ {if ($1 != toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> $report_file
+cat $info_file | awk -F "=" '/drain|drng|maint/ {if ($1 != toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> $report_file 2>&1
 echo >> $report_file
 echo "=== DRAIN ON PURPOSE ===" >> $report_file
-cat $info_file | awk -F "=" '/drain|drng|maint/ {if ($1 == toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> $report_file
+cat $info_file | awk -F "=" '/drain|drng|maint/ {if ($1 == toupper($1)) printf "%-23.20s%-7.5s%-22s%-8s%s\n", $1,$2,$3,$4,$5}' >> $report_file 2>&1
 echo >> $report_file
 echo "=== Jobs stuck in CG State ===" >> $report_file
-#pipe through awk in case job_name has "CG" in string?
-#squeue | awk '$5 == "CG"' >> $report_file
-squeue | grep CG | tr -s ' ' | awk '{printf "%-9s%-11.9s%-12.10s%-10.8s%-4s%-9.7s%s\n",$1,$2,$3,$4,$5,$6,$8}' >> $report_file
+#pipe through awk in case job_name has "CG" in string? yes!
+squeue | tr -s ' ' | awk -F " " '{if ($5 == "CG") printf "%-9s%-11.9s%-12.10s%-10.8s%-4s%-9.7s%s\n",$1,$2,$3,$4,$5,$6,$8}' >> $report_file 2>&1
+
+#squeue | grep CG | tr -s ' ' | awk '{printf "%-9s%-11.9s%-12.10s%-10.8s%-4s%-9.7s%s\n",$1,$2,$3,$4,$5,$6,$8}' >> $report_file 2>&1
 echo >> $report_file
 echo "=== Active Reservations ===" >> $report_file
-sinfo -T | grep ACTIVE >> $report_file
+sinfo -T | grep ACTIVE >> $report_file 2>&1
 
 # send report to mail-list or echo
 if [[ $nomail == true ]]
@@ -81,4 +82,4 @@ else
 fi
 
 # trap statement to rm temp files on exit
-trap cleanup EXIT
+trap 'rm -r $dir0' EXIT
--- mktemp-trap-test.sh
+++ mktemp-trap-test.sh
@@ -3,12 +3,17 @@
 #testing commands
 
 cleanup(){
-	rm $file0
-	rm $file1
+#	rm $file0
+#	rm $file1
+	rm -r $dir0
 }
 
-file0=$(mktemp `basename $0`.XXXXX.txt)
-file1=$(mktemp `basename $0`.XXXXX)
+dir0=$(mktemp -d `basename $0`.XXX)
+file0=$(mktemp $dir0/XXXXX.txt)
+file1=$(mktemp $dir0/`basename $0`.XXXXX.txt)
+
+echo "this is a temp file named $file0." > $file0
+echo "this is a temp file called $file1." > $file1
 
 echo "this file is named: "$file0
 echo "this file is named: "$file1
@@ -16,6 +21,7 @@ echo "this file is named: "$file1
 cat $file0
 cat $file1
 
-ls
+ls -ahl
+ls -ahl $dir0/
 
 trap cleanup EXIT
