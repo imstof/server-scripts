271035aa3ecdc295effec8823a472be9c1012fe8
imstof <cehnstrom@techsquare.com>
2018-03-23 11:44:10 -0400

    sdhfo

--- 10g-pxe-config.sh
+++ 10g-pxe-config.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+!/bin/bash
 
 #get FQDDs for device and bios
 DEV0=$(racadm get nic.nicconfig | awk '/Mezzanine/ {print $1;exit}')
 DEV1=$(racadm get nic.nicconfig | awk -F'[=#]' '{print $2;exit}')
-if [[ $(racadm get bios.biosbootsettings | awk -F = '/BootMode/ {print $2;exit}') == "Uefi" ]]
+if [[ $(racadm get bios.biosbootsettings | awk -F = '/BootMode/ {print $2}') == "Uefi" ]]
 then
 	BIOS=$(racadm get bios.networksettings.pxedev1endis  | awk -F'[=#]' '{print $2;exit}')
 fi
