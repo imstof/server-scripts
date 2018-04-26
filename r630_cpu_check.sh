fc61612809c5fa532e3aa33e36b2553acbfb0c15
imstof <cehnstrom@techsquare.com>
2018-04-26 09:37:44 -0400

    dsflk

---
+++ r630_cpu_check.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+times=$(clush -wnode[236-308] '{ time echo 2^2^20 | bc -l > /dev/null ; } 2>&1' | grep real | sed 's/real//' | sed 's/\t//g' | sed 's/ //g')
+
+for node in $times
+do
+	if [[ $(echo $(echo $node | awk -F'[ms]' '{print $2}')">6" | bc -l) -eq 1 ]]
+	then nodes=$(echo $nodes $(echo $node | cut -d':' -f1))
+	fi
+done
+
+echo $nodes | nodeset -f
