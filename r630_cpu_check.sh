d8b76e175b97bd02d1ccb179a42e280c0e51c672
imstof <cehnstrom@techsquare.com>
2018-04-26 09:45:31 -0400

    sdasdfa

--- r630_cpu_check.sh
+++ r630_cpu_check.sh
@@ -4,7 +4,7 @@ times=$(clush -wnode[236-308] '{ time echo 2^2^20 | bc -l > /dev/null ; } 2>&1' 
 
 for node in $times
 do
-	if [[ $(echo $(echo $node | awk -F'[ms]' '{print $2}')">6" | bc -l) -eq 1 ]]
+	if [[ $(echo $(echo $node | awk -F'[ms]' '{print $2}')">10" | bc -l) -eq 1 ]]
 	then nodes=$(echo $nodes $(echo $node | cut -d':' -f1))
 	fi
 done
