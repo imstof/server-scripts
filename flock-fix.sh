609f3a6be2fc5c7e9709829989e87652dc193666
imstof <cehnstrom@techsquare.com>
2018-05-29 14:35:20 -0400

    :dklh

---
+++ flock-fix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sed -i 's/localflock/flock/' /etc/fstab
+umount /c3ddb/lustre/cstor01
+umount -a -t lustre
+mount -a -t lustre
+mount /c3ddb/lustre/cstor01
+
+#check if any did not change
+echo $(mount | grep localflock)
