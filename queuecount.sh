f60c6c5b240fc651e741126f4d1f82b40951250d
imstof <imstof@login001.cm.cluster>
2019-07-18 14:20:59 -0400

    dlkh

--- queuecount.sh
+++ queuecount.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-for luser in $(squeue -h | awk '{print $4}' | sort -u)
+for user in $(squeue -h | awk '{print $4}' | sort -u)
 do
+	luser=$(echo "user; list user" | cmsh | grep $user | awk '{print $1}')
 	echo -n $luser" "
 	squeue -hu $luser | wc -l
 done
