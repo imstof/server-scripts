78b437bd1ec0af084b692a3a4f088581b068752e
imstof <cehnstrom@techsquare.com>
2019-08-21 10:27:47 -0400

    Wed Aug 21 10:27:45 EDT 2019

--- job_fail_monitor.sh
+++ job_fail_monitor.sh
@@ -6,7 +6,10 @@ for node in $nodes
 do
 	file0=/home/imstof/job_fail_watch/$node-$(date +%Y%m%d)
 	file1=/home/imstof/job_fail_watch/$node-$(date -d yesterday +%Y%m%d)
+	file2=/home/imstof/job_fail_watch/$node-$(date -d "2 days ago" +%Y%m%d)
 	sacct -N $node -S $(date -d "last week" +%Y-%m-%d) -s NF > $file0
 	[[ -e $file1 ]] || break
 	[[ -n $(diff $file0 $file1) ]] && mail -s "$node job fail" cehnstrom@techsquare.com
+	[[ -e $file2 ]] || break
+	rm $file2
 done
