6ddffdaa076b07ad684be00214192cfec1aa3c4b
imstof <cehnstrom@techsquare.com>
2019-08-27 09:05:25 -0400

    Tue Aug 27 09:05:24 EDT 2019

--- log_reboots.sh
+++ log_reboots.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo $(date) >> /home/imstof/reboots
-/cm/shared/admin/bin/slurm-daily-node-status -t | awk '/Not Responding/ {print $6}' >> /home/imstof/reboots
+/cm/shared/admin/bin/slurm-daily-node-status -t | awk '/Not responding/ {print $6}' >> /home/imstof/reboots
