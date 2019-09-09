d446b7fecee09ea88ad6f1c0cd87f336cd80cc9d
imstof <imstof@login002.cm.cluster>
2019-09-09 14:32:20 -0400

    Mon Sep  9 14:32:20 EDT 2019

--- queuecount.sh
+++ queuecount.sh
@@ -3,6 +3,9 @@
 for user in $(squeue -h | awk '{print $4}' | sort -u)
 do
 	luser=$(echo "user; list user" | cmsh | grep $user | awk '{print $1}')
-	echo -n $luser" "
-	squeue -hu $luser | wc -l
+	for userq in $luser
+	do
+		echo -n $userq" "
+		squeue -hu $userq | wc -l
+	done
 done
