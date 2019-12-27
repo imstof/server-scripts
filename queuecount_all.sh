6587b1e988186c9a95e609a01d167e3972c2abb3
Christophe Ehnstrom <cehnstrom@techsquare.com>
2019-12-27 12:46:02 -0500

    Fri Dec 27 12:46:02 EST 2019

---
+++ queuecount_all.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#adapted to count jobs in various states per user
+
+#help function
+#show_help(){
+#	echo
+#	echo "Usage: `basename $0` [option]"
+#	echo "Summarize number of jobs by user."
+#	echo
+#	echo "  -t JOB_STATE"
+#	echo "		Show only jobs in certain state. Any squeue job state code is viable."
+#	echo "  -h"
+#	echo "		Display this help and exit."
+#	echo
+#}
+#
+#while getopts :ht: opt
+#do
+#	case $opt in
+#		t)
+#			job_state=$OPTARG
+#			;;
+#		h)
+#			show_help
+#			exit 0
+#			;;
+#		\?)
+#			echo "Invalid option -$OPTARG"
+#			echo "Try `basename $0` -h for help"
+#			exit 1
+#			;;
+#	esac
+#done
+
+for user in $(squeue -h | awk '{print $4}' | sort -u)
+do
+	luser=$(echo "user; list user" | cmsh | grep $user | awk '{print $1}')
+	for userq in $luser
+	do
+		echo -n $userq" "
+#		[[ -z $job_state ]] && squeue -hu $userq | wc -l || squeue -hu $userq -t $job_state | wc -l
+		echo -n "PD: $(squeue -hu $userq -tpd | wc -l)  "
+		echo -n "R: $(squeue -hu $userq -tr | wc -l)  "
+		echo "CG: $(squeue -hu $userq -tcg | wc -l)"
+	done
+done
