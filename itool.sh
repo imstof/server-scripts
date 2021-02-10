60aa82642c18dbab97b96a147e0b0497dc13f988
Christophe Ehnstrom <cehnstrom@techsquare.com>
2021-02-10 09:21:08 -0500

    Wed Feb 10 09:21:08 EST 2021

--- itool.sh
+++ itool.sh
@@ -3,5 +3,8 @@
 #default ipmi creds with ipmitool
 #taks 2 args. nodename && "ipmi command"
 
-ipmitool -Ilanplus -Uroot -Pcalvin -H $1.ipmi.cluster $2
+for node in $(echo $1.ipmi.cluster | nodeset -e)
+do
+	ipmitool -Ilanplus -Uroot -Pcalvin -H $node $2
+done
 
