b4f89e0c0777ac9adbf7b7c5d5def097bb8f33a9
Christophe Ehnstrom <cehnstrom@techsquare.com>
2021-02-22 09:19:48 -0500

    Mon Feb 22 09:19:47 EST 2021

--- kapow.sh
+++ kapow.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
+#if powering off first check state && queue
+
+if [[ $2 == "f" ]]
+then
+echo checking nodes...
+scontrol show node $1 | grep State
+squeue -w $1
+read -t 10 -p "OK to power off? " yn
+[[ $? -gt "128" ]] && echo aborting && exit 0
+[[ $yn == "y" ]] &&
+ipmipower -Dlanplus -uroot -pcalvin -h $1.ipmi.cluster -$2 ||
+echo aborting
+exit 0
+else
 ipmipower -Dlanplus -uroot -pcalvin -h $1.ipmi.cluster -$2
+fi
